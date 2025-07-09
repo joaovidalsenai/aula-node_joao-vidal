bed91bb1d1dd36a13e6a03e66ae73d2284ec2937
joaovidalsenai <joao.vidal@ba.estudante.senai.br>
2025-07-09 15:12:06 -0300

    Atividades

---
+++ atividade1.js
@@ -0,0 +1,9 @@
+let idade = parseInt(prompt("Digite sua idade:"));
+    
+if (idade >= 18) {
+    console.log("Maior de idade");
+    alert("Maior de idade");
+} else {
+    console.log("Menor de idade");
+    alert("Menor de idade");
+}
