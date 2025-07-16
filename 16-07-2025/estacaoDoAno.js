2014bb0164f4bb12eaaf3131e969e06dc7e8af55
joaovidalsenai <joao.vidal@ba.estudante.senai.br>
2025-07-16 17:23:53 -0300

    Estação do ano

---
+++ 16-07-2025/estacaoDoAno.js
@@ -0,0 +1,13 @@
+let numero = 2;
+
+if (numero === 1) {
+  console.log("Verão");
+} else if (numero === 2) {
+  console.log("Outono");
+} else if (numero === 3) {
+  console.log("Inverno");
+} else if (numero === 4) {
+  console.log("Primavera");
+} else {
+  console.log("Número inválido! Por favor, digite um número entre 1 e 4.");
+}
