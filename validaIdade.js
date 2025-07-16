32c79fd5478bea0933ae2c8ae72c2b3d7fc4cf12
joaovidalsenai <joao.vidal@ba.estudante.senai.br>
2025-07-16 17:16:41 -0300

    Update validaIdade.js

--- validaIdade.js
+++ validaIdade.js
@@ -1,14 +1,18 @@
-let idade = Math.floor(Math.random() * 100);
-let message = "IDADE INVÁLIDA"
+let idade = 18
 
-if (idade >= 1 && idade <= 11) {
-    message = "Infantil A"
-} else if (idade <= 17) {
-    message = "Jovem"
-} else if (idade <= 50) {
-    message = "Adulto"
+if (idade >= 0 && idade < 7) {
+    console.log("Infância");
+} else if (idade >= 7 && idade < 10) {
+    console.log("Segunda Infância");
+} else if (idade >= 10 && idade < 15) {
+    console.log("Pré-Adolescência");
+} else if (idade >= 15 && idade < 20) {
+    console.log("Adolescência");
+} else if (idade >= 20 && idade < 30) {
+    console.log("Juventude");
+} else if (idade >= 30 && idade <= 60) {
+    console.log("Adulta");
 } else {
-    message = "Idoso"
+    console.log("Faixa etária não classificada");
 }
-
-console.log(`Idade: ${idade}; Classificação: ${message}`)
+  
