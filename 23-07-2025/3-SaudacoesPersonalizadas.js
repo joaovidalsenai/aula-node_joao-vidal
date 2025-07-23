873016692faa420a599c9fba250174e1ebf5292e
joaovidalsenai <joao.vidal@ba.estudante.senai.br>
2025-07-23 17:02:37 -0300

    Correção

--- 23-07-2025/3-SaudacoesPersonalizadas.js
+++ 23-07-2025/3-SaudacoesPersonalizadas.js
@@ -3,5 +3,3 @@ const nomes = ["Ana", "Carlos", "Maria", "João", "Fernanda", "Pedro"];
 for (const nome of nomes) {
     console.log(`Olá, ${nome}! Seja muito bem-vindo(a)!`);
 }
-
-export { nomes };
