5b68163348712206550ced0c3e9422b24a97f94b
joaovidalsenai <joao.vidal@ba.estudante.senai.br>
2025-07-30 14:46:11 -0300

    Update while.js

--- 30-07-2025/estruturas de repeticao/while.js
+++ 30-07-2025/estruturas de repeticao/while.js
@@ -2,6 +2,7 @@ let sum = 0;
 console.log("Soma até o numero ser maior que 10\nValor inicial: 0");
 while(sum <= 10){
     let add = Math.random();
+    sum+=add;
     console.log(`+${add}`);
 }
 console.log(`=${sum}`);
